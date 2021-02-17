23fbbd3a8739b6869fba4577aa90cd6403f7c04f
Tuomas Borman <tvborm@utu.fi>
2021-02-17 18:26:28 +0200

    Main functionality implemented

--- R/plotSeries.R
+++ R/plotSeries.R
@@ -51,7 +51,8 @@ setGeneric("plotSeries", signature = c("x"),
            function(x,
                     abund_values = NULL,
                     x_axis = NULL,
-                    y_axis = NULL)
+                    y_axis = NULL,
+                    rank = NULL)
                standardGeneric("plotSeries"))
 
 
@@ -61,7 +62,8 @@ setMethod("plotSeries", signature = c(x = "SummarizedExperiment"),
           function(x,
                    abund_values = NULL,
                    x_axis = NULL,
-                   y_axis = NULL){
+                   y_axis = NULL,
+                   rank = NULL){
               
               ###################### Input check #######################
               # Check abund_values
@@ -73,9 +75,13 @@ setMethod("plotSeries", signature = c(x = "SummarizedExperiment"),
               }
               
               # Check y_axis
-              if(!(y_axis %in% rownames(x)) ){
-                  stop("'y_axis' must be in rownames(x).", call. = FALSE)
+              # If y_axis is not null, user has specified it
+              if (!is.null(y_axis)){
+                  if(!(y_axis %in% rownames(x)) ){
+                      stop("'y_axis' must be in rownames(x).", call. = FALSE)
+                  }
               }
+              
               
               # Check rank
               # IF rank is not null, it has been specified by user
@@ -89,81 +95,49 @@ setMethod("plotSeries", signature = c(x = "SummarizedExperiment"),
               }
               ##############################################
               
+              # If rank is not null, data will be agglomerated by rank
+              if( !is.null(rank) ){
+                  x <- agglomerateByRank(x, rank = rank)
+                  
+                  # Rownames are now changed, so y-axis must be checked again
+                  # Check y_axis
+                  if (!is.null(y_axis)){
+                      if(!(y_axis %in% rownames(x)) ){
+                          stop("'y-axis' does not match to rownames(x). 
+                          Data is agglomerated by 'rank', and rownames(x) are
+                          also changed in relation to 'rank'. Check that 'y-axis'
+                          match to agglomerated data.", call. = FALSE)
+                      }
+                  }
+              }
+              
+              # If y_axis is not null, take only those taxa into account that user
+              # is specified
+              if( !is.null(y_axis) ){
+                  x <- x[y_axis]
+              }
+              
+              # Melt data
+              melted_data <- meltAssay(x, add_col_data = TRUE, add_row_data = TRUE)
+              color_by = "Kingdom"
+              line_by = "FeatureID"
+              
+              # # Take only those columns that are needed
+              # melted_data <- melted_data[, c("FeatureID", abund_values, x_axis)]
+              
+              ggplot2::ggplot(melted_data) + 
+                  ggplot2::geom_line(
+                      ggplot2::aes_string(x = x_axis, y = abund_values, 
+                                          color = color_by, linetype = line_by))
               
               
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              
-              # Nothing ready yet #############################
-              
-              # Load moving pictures, it includes time points
-              
-              #devtools::load_all()
-              
-              mp <- moving_pictures
-              # Create TSE from it
-              TSE <- mia::makeTreeSummarizedExperimentFromphyloseq(mp)
-              colData(TSE)
-              ########################################################
-              
-              # Transforms abundance table to relative (there could be an option for relative or absolute values)
-              TSE <- mia::transformCounts(TSE, method = "relabundance")
-              
-              # Get the names of most abundant taxa
-              core_taxa <- mia::getTopTaxa(TSE, top = 5)
-              core_taxa
-              
-              
-              # Melt data to get a data frame PROBLEM: when creating molted df, it does not include colData, all values are NA. Problem in meltAssay?
-              # I am not yet 100 % sure that TSE[core_taxa] works as intended. The purpose of this is to take only those taxa that are in the core taxa. 
-              # It seems to work, but I have to check more carefully and with other data
-              molten_se <- mia::meltAssay(TSE[core_taxa],
-                                          add_row_data = TRUE,
-                                          add_col_data = TRUE,
-                                          abund_values = "relabundance")
-              
-              # Create a plot. days in x-axis and abundance in y-axis
-              ggplot2::ggplot(na.omit(as.data.frame(molten_se))) + 
-                  ggplot2::geom_line(ggplot2::aes(days_since_experiment_start, 
-                                                  relabundance, color = FeatureID)) +
-                  theme_bw() + 
-                  theme(legend.position="top") + 
-                  xlab("Days since experiment start") + 
-                  ylab("Relative abundance") + 
-                  scale_color_brewer("Core ASVs",palette = "Paired") +
-                  guides(col = guide_legend(ncol = 3, nrow = 3))
+                  #     ) +
+                  # theme_bw() + 
+                  # theme(legend.position="top") + 
+                  # xlab(x_axis) + 
+                  # ylab(abund_values) + 
+                  # scale_color_brewer("Core ASVs",palette = "Paired") +
+                  # guides(col = guide_legend(ncol = 3, nrow = 3))
               
               
               
