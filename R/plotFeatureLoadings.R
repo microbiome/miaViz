eb814a8731a581030bbbb3a9bf45f10e8c2d3ab6
seraidarian <ely.seraidarian@ecole.ensicaen.fr>
2024-05-23 11:45:17 +0300

    Creating function's body

---
+++ R/plotFeatureLoadings.R
@@ -0,0 +1,39 @@
+#' Plot tree with feature loadings after performing a reducedDim (e.g. PCA, PCoA, NMDS)
+#'
+#' Based on the \code{\link[diffTop:plotASVcircular]{plotASVcircular}} method using phyloseq 
+#' and has been converted to use TreeSummarizedExperiment objects.
+#'
+#' @param object a
+#'   \code{\link[TreeSummarizedExperiment:TreeSummarizedExperiment-class]{TreeSummarizedExperiment}}
+#'   object.
+#' 
+#' @param K An integer to specify the number of feature loadings to plot.
+#'   (default: \code{K = 2})
+#' 
+#' @param taxonomyLevel 
+#' # ADD ALL PARAMS
+#' 
+#' @details
+#' # TO DO 
+#' @return 
+#' A \code{ggplot2} object. A circular plot annotated with TreeSummarizedExperiment object.
+#'
+#' @name plotFeatureLoadings
+#'
+#' @examples
+#' # TO DO
+NULL
+
+#' @rdname plotFeatureLoadings
+setGeneric("plotFeatureLoadings", signature = c("object"),
+           function(object, ...)
+             standardGeneric("plotFeatureLoadings"))
+
+
+#' @rdname plotTree
+#' @export
+setMethod("plotFeatureLoadings", signature = c(object = "TreeSummarizedExperiment"),
+          function(object,
+                   ...){
+            
+}
