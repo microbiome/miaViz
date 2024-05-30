9d3edf2bde8e74774b552c4623d62083b074df73
thpralas <theotime.pralas@centrale-med.fr>
2024-05-30 10:46:52 +0300

    add minimum documentation for plotNMDS

--- R/plotNDMS.R
+++ R/plotNDMS.R
@@ -1,3 +1,13 @@
+#' Wrapper for scater::plotReducedDim()
+#' 
+#' @param x a
+#'   \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}}
+#'   object.
+#'   
+#' @name plotNMDS
+NULL
+
+#' @rdname plotNMDS
 #' @export
 plotNMDS <- function(x, ..., ncomponents = 2){
     plotReducedDim(x, ncomponents = ncomponents, dimred = "NMDS", ...)
