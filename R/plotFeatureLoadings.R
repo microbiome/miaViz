d7bb4a7aceb99448304c1485be9ad668c2ed1c3a
seraidarian <ely.seraidarian@ecole.ensicaen.fr>
2024-06-10 15:34:29 +0300

    Added example & removed prints statements

--- R/plotFeatureLoadings.R
+++ R/plotFeatureLoadings.R
@@ -22,71 +22,13 @@
 #' @name plotFeatureLoadings
 #'
 #' @examples
-#' 
-#' 
-#' 
-# library(mia)
-# library(ggtree)
-# library(scater)
-# data("GlobalPatterns", package = "mia")
-# tse <- GlobalPatterns
-# tse <- logNormCounts(tse)
-# tse <- tse[rowData(tse)$Phylum %in% c("Actinobacteria", "Caldiserica","Synergistetes","Crenarchaeota","Euryarchaeota"), ]
-# tse <- agglomerateByRank(tse, rank = "Class", agglomerate.tree = TRUE)
-# tse <- runPCA(tse, name = "PCA", ncomponents = 2, ntop = 100)
-# loadings_matrix <- attr(reducedDim(tse,"PCA"), "rotation")
-# phylo <- rowTree(tse)
-# circ <- ggtree(phylo, layout = "circular")
-# df <- rowData(tse)
-# color <- randomcoloR::distinctColorPalette(
-#   length(
-#     unique(
-#       df$Phylum
-#     )
-#   )
-# )
-# df <- data.frame(Class = df$Phylum)
-# rownames(df) <- phylo$tip.label
-# 
-# df2 <- data.frame(loadings_matrix)
-# rownames(df2) <- phylo$tip.label
-# 
-# p <- gheatmap(
-#   p = circ,
-#   data = df,
-#   offset = -.1,
-#   width = .1,
-#   colnames_angle = 95,
-#   colnames_offset_y = .5,
-#   font.size = 4,
-#   color = "black") +
-#   ggplot2::scale_fill_manual(
-#     values = color,
-#     name = "Class"
-#   )
-# 
-# for(i in 1:2){
-#   if(i == 1){
-#     p <- p +
-#       ggnewscale::new_scale_fill()
-#   }
-#   df3 <- dplyr::select(
-#     df2, (all_of(i))
-#   )
-#  p <- gheatmap(
-#     p,
-#     df3,
-#     offset = i*.065,
-#     width = .1,
-#     colnames_angle = 90,
-#     font.size = 4,
-#     high = "darkslateblue",
-#     low = "gray98",
-#     color = "black",
-#     legend_title = expression(beta[k]))
-# }
-# 
-# p
+#' library(mia)
+#' library(ggtree)
+#' library(scater)
+#' data("GlobalPatterns", package = "mia)
+#' tse <- GlobalPatterns
+#' tse <- logNormCounts(tse)
+#' plotFeatureLoadings(tse[rowData(tse)$Phylum %in% c("Actinobacteria", "Caldiserica", "Synergistetes", "Crenarchaeota", "Euryarchaeota"),], method= "PCA")
 NULL
 
 #' @rdname plotFeatureLoadings
@@ -139,10 +81,6 @@ setMethod("plotFeatureLoadings", signature = c(object = "TreeSummarizedExperimen
     phylo <- rowTree(object)
     circ <- ggtree(phylo, layout = "circular")
     df <- rowData(object)
-    print(rownames(df))
-    print(rowTree(object))
-    print(nrow(object))
-    
     color <- randomcoloR::distinctColorPalette(
         length(
             unique(
