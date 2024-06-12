b81c388ddb0fca7ff02698a158bacffee95c3416
thpralas <theotime.pralas@centrale-med.fr>
2024-06-12 11:29:53 +0300

    document arguments of plotNMDS

--- R/plotNDMS.R
+++ R/plotNDMS.R
@@ -4,6 +4,13 @@
 #'   \code{\link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment}}
 #'   object.
 #'   
+#' @param ncomponents 	
+#'   A numeric scalar indicating the number of dimensions to plot, starting from 
+#'   the first dimension. Alternatively, a numeric vector specifying the 
+#'   dimensions to be plotted. (Default: 2)
+#'   
+#' @param ... additional arguments passed to scater::plotReducedDim()
+#'   
 #' @name plotNMDS
 NULL
 
