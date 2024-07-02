97814eadef3db9df5f9f71fdb1955329d751f268
seraidarian <ely.seraidarian@ecole.ensicaen.fr>
2024-07-02 14:36:50 +0300

    All working examples + added heatmap (format to define later)

--- R/plotFeatureLoadings.R
+++ R/plotFeatureLoadings.R
@@ -9,10 +9,13 @@
 #' 
 #' @param method One dimensionality reduction method of \code{c("PCA", "MDS", "PCoA", "NMDS")}
 #'   indicating which method will be used.
-#'   (default: \code{method = "PCA})
+#'   (default: \code{method = "PCA"})
 #'  
 #' @param rank a single character defining a taxonomic rank. Must be a value of
 #'   \code{taxonomyRanks()} function.
+#'   
+#' @param heatmap a boolean indicating whether or not to plot heatmap 
+#'   (default: \code{heatmap = FALSE})
 #'   
 #' @details
 #' # TO DO 
@@ -25,113 +28,135 @@
 #' library(mia)
 #' library(ggtree)
 #' library(scater)
-#' data("GlobalPatterns", package = "mia)
+#' data("GlobalPatterns", package = "mia")
 #' tse <- GlobalPatterns
 #' tse <- logNormCounts(tse)
-#' plotFeatureLoadings(tse[rowData(tse)$Phylum %in% c("Actinobacteria", "Caldiserica", "Synergistetes", "Crenarchaeota", "Euryarchaeota"),], method= "PCA")
+#' tse <- agglomerateByPrevalence(tse, rank="Phylum", prevalence=0.99, update.tree = TRUE)
+#' plotFeatureLoadings(tse, method= "PCA")
 NULL
 
 #' @rdname plotFeatureLoadings
 setGeneric("plotFeatureLoadings", signature = c("object"),
-          function(object, ...)
+           function(object, ...)
              standardGeneric("plotFeatureLoadings"))
 
 
-#' @rdname plotTree
+#' @rdname plotFeatureLoadings
 #' @export
 setMethod("plotFeatureLoadings", signature = c(object = "TreeSummarizedExperiment"),
-    function(object,
-            method = "PCA", rank = "Class",
-            ...){
-
-    .check_parameters(object,
-                    method = method,...)    
-      
-           if (rank %in% taxonomyRanks(object)) {
-             object <- agglomerateByRank(object, rank = "Class", agglomerate.tree = TRUE)
-           }
-           object <- .run_reduction_method(object, method = method)
-           
-           if (method == "PCA") {
+          function(object,
+                   method = "PCA", heatmap = FALSE,
+                   ...){
+            
+            .check_parameters(object,
+                              method = method,...)   
+            
+            object <- .run_reduction_method(object, method = method)
+            
+            if (method == "PCA") {
               loadings_matrix <- attr(reducedDim(object, "PCA"), "rotation")
-           } else {
-             stop("These methods are yet to be implemented", call. = FALSE)
-           }
-
-           .plot_feature_loadings(object,loadings_matrix, Phylum)
-})
+            } else {
+              stop("These methods are yet to be implemented", call. = FALSE)
+            }
+            
+            .plot_feature_loadings(object, loadings_matrix, heatmap)
+          })
 
 .check_parameters <- function(object, method, ...) {
-    
-    if ( !(method %in% c("PCA", "MDS", "NMDS", "PCoA")) ) {
-        stop("'method' must be one of c('PCA', 'MDS', 'NMDS', 'PCoA').", call. = FALSE)
-    }
+  
+  if ( !(method %in% c("PCA", "MDS", "NMDS", "PCoA")) ) {
+    stop("'method' must be one of c('PCA', 'MDS', 'NMDS', 'PCoA').", call. = FALSE)
+  }
 }
 
 .run_reduction_method <- function(object, method) {
-    if (method == "PCA") {
-        object <- runPCA(object, name = "PCA", ncomponents = 2)
-    } else {
-        stop("These methods are yet to be implemented", call. = FALSE)
-    }
+  if (method == "PCA") {
+    object <- runPCA(object, name = "PCA", ncomponents = 2)
+  } else {
+    stop("These methods are yet to be implemented", call. = FALSE)
+  }
   object
 }
 
-.plot_feature_loadings <- function(object,loadings_matrix, class) {
-    phylo <- rowTree(object)
-    circ <- ggtree(phylo, layout = "circular")
-    df <- rowData(object)
-    color <- randomcoloR::distinctColorPalette(
-        length(
-            unique(
-                df$Phylum
-            )
-        )
+.plot_feature_loadings <- function(object, loadings_matrix, heatmap) {
+  phylo <- rowTree(object)
+  circ <- ggtree(phylo, layout = "circular")
+  df <- rowData(object)
+  color <- randomcoloR::distinctColorPalette(
+    length(
+      unique(
+        df$Phylum
+      )
     )
-    df <- data.frame(Class = df$Phylum)
-    rownames(df) <- phylo$tip.label
-    
-    df2 <- data.frame(loadings_matrix)
-    rownames(df2) <- phylo$tip.label
+  )
+  df <- data.frame(Class = df$Phylum)
+  rownames(df) <- phylo$tip.label
   
-    p <- gheatmap(
-        p = circ,
-        data = df,
-        offset = -.1,
-        width = .1,
-        colnames_angle = 95,
-        colnames_offset_y = .5,
-        font.size = 4,
-        color = "black") +
-        ggplot2::scale_fill_manual(
-            values = color,
-            name = "Class"
-        )
+  if (heatmap) {
+    df2 <- data.frame(loadings_matrix,phylo$tip.label)
+    print(loadings_matrix)
     
-    for(i in 1:2){
-        if(i == 1){
-            p <- p +
-            ggnewscale::new_scale_fill()
-        }
-        df3 <- dplyr::select(
-            df2, all_of(i)
-        )
-        p <- gheatmap(
-            p,
-            df3,
-            offset = i*.065,
-            width = .1,
-            colnames_angle = 90,
-            font.size = 4,
-            high = "darkslateblue",
-            low = "gray98",
-            color = "black",
-            legend_title = expression(beta[k])
-            )
+    maxval <- max(abs(df3$X1))
+    limits <- c(-maxval, maxval)
+    breaks <- seq(from = min(limits), to = max(limits), by = 0.000001)
+    colours <- c("white","blue", "darkblue")
+    
+    
+    ggplot(df2 , aes(x = phylo.tip.label, y = rownames(df2), fill = PC2)) +
+      geom_tile() +
+      theme(text = element_text(size=10),
+            axis.text.x = element_text(angle=45, hjust=1),
+            legend.key.size = unit(1, "cm")) +
+      labs(x = "Features", y = "Taxa")
+  }
+  
+  df2 <- data.frame(abs(loadings_matrix))
+  rownames(df2) <- phylo$tip.label
+  
+  
+  p <- gheatmap(
+    p = circ,
+    data = df,
+    offset = -.1,
+    width = .1,
+    colnames_angle = 95,
+    colnames_offset_y = .5,
+    font.size = 4,
+    color = "black") +
+    ggplot2::scale_fill_manual(
+      values = color,
+      name = "Class"
+    )
+  
+  for(i in 1:2){
+    if(i == 1){
+      p <- p +
+        ggnewscale::new_scale_fill()
     }
+    df3 <- dplyr::select(
+      df2, (all_of(i))
+    )
+    
+    p <- gheatmap(
+      p,
+      df3,
+      offset = i*.065,
+      width = .1,
+      colnames_angle = 90,
+      font.size = 4,
+      high = "darkslateblue",
+      low = "gray98",
+      color = "black",
+      legend_title = expression(beta[k]))
+  }
   
-    p
+  p
+  
+  
 }
+# 
+# 
+# 
 # 
 # 
 # 
@@ -145,8 +170,7 @@ setMethod("plotFeatureLoadings", signature = c(object = "TreeSummarizedExperimen
 # data(GlobalPatterns)
 # tse <- GlobalPatterns
 # tse <- transformAssay(tse, method = "relabundance")
-# tse <- agglomerateByPrevalence(tse, rank="Genus", assay.type="relabundance",detection = 1/100, prevalence=5/100)
-# tse <- tse[!(rownames(tse) %in% c("Other")),]
+# tse <- agglomerateByPrevalence(tse, rank="Phylum", prevalence=0.99, update.tree = TRUE)
 # x <- t(assay(tse, "counts"))
 # nmf2 <- nmf(x, 2)
 # H <- nmf2@fit@H
@@ -205,12 +229,12 @@ setMethod("plotFeatureLoadings", signature = c(object = "TreeSummarizedExperimen
 # }
 # 
 # p
-# 
-#  
-# 
-# 
-# 
-# 
+# # 
+# #  
+# # 
+# # 
+# # 
+# # 
 # library(mia)
 # library(topicmodels)
 # 
@@ -218,6 +242,8 @@ setMethod("plotFeatureLoadings", signature = c(object = "TreeSummarizedExperimen
 # tse <- GlobalPatterns
 # 
 # tse <- transformAssay(tse, method = "relabundance")
+# 
+# tse <- agglomerateByPrevalence(tse, rank = "Class", update.tree = TRUE, prevalence = 0.99)
 # 
 # lda_model <- LDA(t(assay(tse, "counts")), k = 2)
 # 
@@ -227,9 +253,6 @@ setMethod("plotFeatureLoadings", signature = c(object = "TreeSummarizedExperimen
 # 
 # feature_loadings <- t(as.data.frame(posteriors$terms))
 # 
-# top_loadings_indices <- order(rowSums(feature_loadings), decreasing = TRUE)[1:30]
-# 
-# feature_loadings_reduced <- feature_loadings[top_loadings_indices, ]
 # 
 # phylo <- rowTree(tse)
 # circ <- ggtree(phylo, layout = "circular")
@@ -244,7 +267,7 @@ setMethod("plotFeatureLoadings", signature = c(object = "TreeSummarizedExperimen
 # df <- data.frame(Class = df$Phylum)
 # rownames(df) <- phylo$tip.label
 # 
-# df2 <- data.frame(feature_loadings_reduced)
+# df2 <- data.frame(feature_loadings)
 # rownames(df2) <- phylo$tip.label
 # 
 # p <- gheatmap(
