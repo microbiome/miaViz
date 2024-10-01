20beb75cb5940163cba799893ffc77d89b0aa8f2
Muluh <127390183+Daenarys8@users.noreply.github.com>
2024-10-01 15:53:24 +0100

    add support for groups other than rank in plotAbundance (#148)
    Signed-off-by: Daena Rys <rysdaena8@gmail.com>
    Co-authored-by: TuomasBorman <tvborm@utu.fi>

--- tests/testthat/test-2plotAbundance.R
+++ tests/testthat/test-2plotAbundance.R
@@ -5,27 +5,26 @@ test_that("plot abundance", {
     data(GlobalPatterns)
     x <- GlobalPatterns
     # .check_tree_plot_switches
-    expect_error(miaViz:::.get_abundance_data(rank = "Phylum"),
+    expect_error(miaViz:::.get_abundance_data(group = "Phylum"),
                  'argument "x" is missing')
-    expect_error(miaViz:::.get_abundance_data(x, rank = "Phylum"),
+    expect_error(miaViz:::.get_abundance_data(x, group = "Phylum"),
                  'argument "assay.type" is missing')
-    expect_error(miaViz:::.get_abundance_data(x),
-                 'argument "rank" is missing')
-    actual <- miaViz:::.get_abundance_data(x,"Phylum","counts")
+    expect_error(miaViz:::.get_abundance_data(x))
+    actual <- miaViz:::.get_abundance_data(x, group = "Phylum", assay.type = "counts")
     expect_s3_class(actual,"tbl_df")
     expect_named(actual,c("colour_by","X","Y"))
     expect_true(is.factor(actual$colour_by))
     expect_true(is.factor(actual$X))
     expect_true(is.numeric(actual$Y))
-    expect_error(miaViz:::.get_abundance_data(x,"Phylum","counts",
-                                              order_rank_by = "meep"))
-    actual2 <- miaViz:::.get_abundance_data(x,"Phylum","counts",
-                                           order_rank_by = "abund")
+    expect_error(miaViz:::.get_abundance_data(
+        x, group = "Phylum", assay.type = "counts", order_rank_by = "meep"))
+    actual2 <- miaViz:::.get_abundance_data(
+        x, group = "Phylum", assay.type = "counts", order_rank_by = "abund")
     expect_equal(as.character(actual[1,1,drop=TRUE]),"ABY1_OD1")
     expect_equal(as.character(actual2[1,1,drop=TRUE]),"Proteobacteria")
-    actual3 <- miaViz:::.get_abundance_data(x,"Phylum","counts",
-                                            order.row.by = "abund",
-                                            as.relative = FALSE)
+    actual3 <- miaViz:::.get_abundance_data(
+        x, group = "Phylum", assay.type = "counts", order.row.by = "abund",
+        as.relative = FALSE)
     expect_true(max(actual3$Y) > 1)
     # .norm_order_sample_by
     expect_true(is.null(miaViz:::.norm_order_sample_by(NULL)))
@@ -50,7 +49,8 @@ test_that("plot abundance", {
     expect_true(is.data.frame(actual))
     expect_named(actual,c("SampleType","Primer"))
     # .order_abund_feature_data
-    abund_data <- miaViz:::.get_abundance_data(x,"Phylum","counts")
+    abund_data <- miaViz:::.get_abundance_data(
+        x, group = "Phylum", assay.type = "counts")
     features_data <- miaViz:::.get_features_data("Primer","SampleType",x)
     expect_error(miaViz:::.order_abund_feature_data(abund_data,features_data),
                  'argument "order_sample_by" is missing')
@@ -69,7 +69,14 @@ test_that("plot abundance", {
     plot <- plotAbundance(x[1:20, ], assay.type="counts")
     expect_s3_class(plot,"ggplot")
     expect_named(plot$data,c("colour_by","X","Y"))
-    plot <- plotAbundance(x, assay.type="counts", rank = "Phylum",
+    plot <- plotAbundance(x, assay.type="counts", group = "Phylum",
+                          col.var = "SampleType",
+                          order.col.by = "SampleType")
+    expect_true(is.list(plot))
+    expect_s3_class(plot[[1]],"ggplot")
+    #
+    rowData(x)$Salame <- sample(letters[1:5], nrow(x), replace=TRUE)
+    plot <- plotAbundance(x, assay.type="counts", group = "Salame",
                           col.var = "SampleType",
                           order.col.by = "SampleType")
     expect_true(is.list(plot))
