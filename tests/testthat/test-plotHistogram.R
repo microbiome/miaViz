4a30490c3742b68bcb316d2084b89db68c31e2d7
TuomasBorman <tvborm@utu.fi>
2025-02-14 17:25:08 +0200

    up

---
+++ tests/testthat/test-plotHistogram.R
@@ -0,0 +1,52 @@
+
+test_that("plotHistogram check input tssts", {
+    tse <- makeTSE(nrow = 10L, ncol = 10L)
+    assayNames(tse) <- "counts"
+    rowData(tse)[["value"]] <- rnorm(nrow(tse))
+    colData(tse)[["value"]] <- rnorm(ncol(tse))
+    #
+    expect_error(
+        plotHistogram(tse),
+        "Please specify either 'assay.type', 'row.var', or 'col.var'.")
+    #
+    expect_error(plotHistogram(
+        tse, row.var = "value", features = rownames(tse)))
+    #
+    expect_error(plotHistogram(
+        tse, col.var = "value", features = rownames(tse)))
+    #
+    expect_error(plotHistogram(tse, assay.type = "test"))
+    #
+    expect_error(plotHistogram(tse, row.var = "test"))
+    #
+    expect_error(plotHistogram(tse, col.var = "test"))
+    #
+    expect_error(plotHistogram(tse, col.var = "value", assay.type = "counts"))
+    #
+    expect_error(plotHistogram(tse, row.var = "value", assay.type = "counts"))
+    #
+    expect_error(plotHistogram(tse, features = "test", assay.type = "counts"))
+    #
+    expect_error(plotHistogram(tse, layout = "test", assay.type = "counts"))
+})
+
+test_that("plotHistogram returns a ggplot object for valid inputs", {
+    tse <- makeTSE(nrow = 10L, ncol = 10L)
+    assayNames(tse) <- "counts"
+    rowData(tse)[["value"]] <- rnorm(nrow(tse))
+    colData(tse)[["value"]] <- rnorm(ncol(tse))
+    # Check for ggplot object when assay.type is specified
+    p <- plotHistogram(tse, assay.type = "counts")
+    expect_s3_class(p, "ggplot")
+
+    # Check for ggplot object when row.var is specified
+    p <- plotHistogram(tse, row.var = "value")
+    expect_s3_class(p, "ggplot")
+
+    # Check for ggplot object when col.var is specified
+    p <- plotHistogram(tse, col.var = "value")
+    expect_s3_class(p, "ggplot")
+
+    # Check that the values are correct
+    expect_equal(tse[["value"]], p$data$value)
+})
