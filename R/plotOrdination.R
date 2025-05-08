44f58039cb6bf3ac814b0ebf1b7c20d5d583704b
TuomasBorman <tvborm@utu.fi>
2025-05-08 10:57:28 +0300

    up

--- R/plotOrdination.R
+++ R/plotOrdination.R
@@ -39,8 +39,9 @@ NULL
 #' @export
 setMethod("plotOrdination", signature = c(x = "SingleCellExperiment"),
     function(x, dimred, colour.by = NULL, ...){
-        temp <- .check_ordination_input(x, ...)
-        df <- .get_ordination_data(x, ...)
+        args <- .check_ordination_input(x, dimred, colour.by = colour.by, ...)
+        df <- do.call(.get_ordination_data, args)
+        return(df)
         p <- .ordination_plotter(df, ...)
         return(p)
     }
@@ -60,7 +61,7 @@ setMethod("plotOrdination", signature = c(x = "SingleCellExperiment"),
         assay.type = "counts",
         ...){
     # Check if there are any reduced dim present
-    if( lenght(reducedDims(x)) == 0L ){
+    if( length(reducedDims(x)) == 0L ){
         stop("No data present in reducedDim(x).", call. = FALSE)
     }
     # Check that dimred can be found
@@ -75,29 +76,72 @@ setMethod("plotOrdination", signature = c(x = "SingleCellExperiment"),
     if( .is_an_integer(ncomponents) ){
         ncomponents <- seq_len(ncomponents)
     }
-    if( !(.is_integer(ncomponents) && length(ncomponents) == 2L) ){
-        stop("'ncomponents' must specify intger values.", call. = FALSE)
+    if( !(.is_integer(ncomponents) && length(ncomponents) == 2L && all(ncomponents > 0L & ncomponents <= ncol(reducedDim(x, dimred)))) ){
+        stop("'ncomponents' must specify columns from reducedDim(x, dimred) with integer values.", call. = FALSE)
     }
 
     # Check aesthetic variables
-    temp <- .check_metadata_variable(tse, colour.by, TRUE, TRUE, FALSE, TRUE)
-    temp <- .check_metadata_variable(tse, shape.by, TRUE, TRUE, FALSE, FALSE)
-    temp <- .check_metadata_variable(tse, size.by, TRUE, TRUE, FALSE, FALSE)
-    temp <- .check_metadata_variable(tse, group.by, TRUE, TRUE, FALSE, FALSE)
-    temp <- .check_metadata_variable(tse, pair.by, TRUE, TRUE, FALSE, FALSE)
-    temp <- .check_metadata_variable(tse, order.by, TRUE, TRUE, FALSE, FALSE)
+    temp <- .check_metadata_variable(tse, colour.by, FALSE, TRUE, FALSE, TRUE)
+    temp <- .check_metadata_variable(tse, shape.by, FALSE, TRUE, FALSE, FALSE)
+    temp <- .check_metadata_variable(tse, size.by, FALSE, TRUE, FALSE, FALSE)
+    temp <- .check_metadata_variable(tse, group.by, FALSE, TRUE, FALSE, FALSE)
+    temp <- .check_metadata_variable(tse, pair.by, FALSE, TRUE, FALSE, FALSE)
+    temp <- .check_metadata_variable(tse, order.by, FALSE, TRUE, FALSE, FALSE)
     # If colour.by specifies rowname, we check assay.type as the abundance
     # values are used for coloring
-    if( colour.by &in& rownames(x) ){
+    if( !is.null(colour.by) && colour.by %in% rownames(x) ){
         temp <- .check_assay_present(assay.type, x)
     }
     # Check other flags
 
+    # Put all the arguments into list that be fed to the data retrieval function
+    args <- list(
+        x = x, dimred = dimred,
+        ncomponents = ncomponents,
+        colour.by = colour.by,
+        shape.by = shape.by,
+        size.by = size.by,
+        group.by = group.by,
+        pair.by = pair.by,
+        order.by = order.by,
+        assay.type = "counts"
+    )
+    args <- c(args, list(...))
+    return(args)
 }
 
-.get_ordination_data <- function(x, dimred, ...){
-    df <- reducedDim(x, dimred) |> as.data.frame()
+.get_ordination_data <- function(x, dimred, ncomponents, colour.by, shape.by, size.by, group.by, pair.by, order.by, assay.type, ...){
+    df <- reducedDim(x, dimred)
+    if( is.null(colnames(df)) ){
+        colnames(df) <- paste0(dimred, seq_len(ncol(df)))
+    }
+    df <- df |> as.data.frame()
     df <- df[, ncomponents]
+    x_var <- colnames(df)[[1L]]
+    y_var <- colnames(df)[[2L]]
+
+    cols <- c(shape.by, size.by, group.by, pair.by, order.by)
+    if( !is.null(colour.by) && colour.by %in% colnames(colData(x)) ){
+        cols <- c(cols, colour.by)
+    } else if( !is.null(colour.by) ){
+        df[[colour.by]] <- assay(x, assay.type)[colour.by, ]
+    }
+    cd <- colData(x)[, cols, drop = FALSE]
+    df <- cbind(df, cd)
+
+    attributes(df) <- c(
+        attributes(df),
+        x = x_var,
+        y = y_var,
+        colour.by = colour.by,
+        shape.by = shape.by,
+        size.by = size.by,
+        group.by = group.by,
+        pair.by = pair.by,
+        order.by = order.by,
+        assay.type = assay.type
+    )
+    return(df)
 }
 
 .ordination_plotter <- function(df, ...){
