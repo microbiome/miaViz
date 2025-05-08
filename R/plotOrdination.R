442d73dc8ec8650639947724f3ff81ea0d1ee362
TuomasBorman <tvborm@utu.fi>
2025-05-08 09:38:32 +0300

    up

--- R/plotOrdination.R
+++ R/plotOrdination.R
@@ -37,8 +37,8 @@ NULL
 
 #' @rdname plotOrdination
 #' @export
-setMethod("plotOrdination", signature = c(x = "SummarizedExperiment"),
-    function(x, ...){
+setMethod("plotOrdination", signature = c(x = "SingleCellExperiment"),
+    function(x, dimred, colour.by = NULL, ...){
         temp <- .check_ordination_input(x, ...)
         df <- .get_ordination_data(x, ...)
         p <- .ordination_plotter(df, ...)
@@ -48,12 +48,56 @@ setMethod("plotOrdination", signature = c(x = "SummarizedExperiment"),
 
 ################################ HELP FUNCTIONS ################################
 
-.check_ordination_input <- function(x, ...){
+.check_ordination_input <- function(
+        x, dimred,
+        ncomponents = 2L,
+        colour.by = color.by, color.by = NULL,
+        shape.by = NULL,
+        size.by = NULL,
+        group.by = NULL,
+        pair.by = NULL,
+        order.by = NULL,
+        assay.type = "counts",
+        ...){
+    # Check if there are any reduced dim present
+    if( lenght(reducedDims(x)) == 0L ){
+        stop("No data present in reducedDim(x).", call. = FALSE)
+    }
+    # Check that dimred can be found
+    is_name <- .is_a_string(dimred) && dimred %in% reducedDimNames(x)
+    is_index <- .is_an_integer(dimred) && dimred > 0L && dimred <= length(reducedDims(x))
+    if( !(is_name || is_index) ){
+        stop("'dimred' must specify data from reducedDim(x). It must be one ",
+            "of the following options: '",
+            paste0(reducedDimNames(x), collapse = "', '"), "'", call. = FALSE)
+    }
+    # Check that ncomponents is correct. We can only visualize 2 components.
+    if( .is_an_integer(ncomponents) ){
+        ncomponents <- seq_len(ncomponents)
+    }
+    if( !(.is_integer(ncomponents) && length(ncomponents) == 2L) ){
+        stop("'ncomponents' must specify intger values.", call. = FALSE)
+    }
+
+    # Check aesthetic variables
+    temp <- .check_metadata_variable(tse, colour.by, TRUE, TRUE, FALSE, TRUE)
+    temp <- .check_metadata_variable(tse, shape.by, TRUE, TRUE, FALSE, FALSE)
+    temp <- .check_metadata_variable(tse, size.by, TRUE, TRUE, FALSE, FALSE)
+    temp <- .check_metadata_variable(tse, group.by, TRUE, TRUE, FALSE, FALSE)
+    temp <- .check_metadata_variable(tse, pair.by, TRUE, TRUE, FALSE, FALSE)
+    temp <- .check_metadata_variable(tse, order.by, TRUE, TRUE, FALSE, FALSE)
+    # If colour.by specifies rowname, we check assay.type as the abundance
+    # values are used for coloring
+    if( colour.by &in& rownames(x) ){
+        temp <- .check_assay_present(assay.type, x)
+    }
+    # Check other flags
 
 }
 
-.get_ordination_data <- function(x, ...){
-
+.get_ordination_data <- function(x, dimred, ...){
+    df <- reducedDim(x, dimred) |> as.data.frame()
+    df <- df[, ncomponents]
 }
 
 .ordination_plotter <- function(df, ...){
